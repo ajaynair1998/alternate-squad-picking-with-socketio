406d9007731465ad48b00aae008661218da52ecb
ajay <ajaynairarun@gmail.com>
2022-06-30 10:16:32 +0530

    add : redis client

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,18 +1,32 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import dotenv from "dotenv";
 import router from "./routes";
-
+import { Server } from "socket.io";
+import cors from "cors";
+import { database } from "./db";
+//initialise connection
+database;
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get("/", (req: Request, res: Response) => {
-	res.send("Express + TypeScript Server started");
-});
-
+app.use(cors());
+app.use(express.json());
 app.use("/", router);
 
-app.listen(port, () => {
+let server = app.listen(port, () => {
 	console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
+
+const io = new Server(server, { cors: { origin: "*" } });
+
+//initializing the socket io connection
+io.on("connection", (socket) => {
+	socket.emit("message", "message from server");
+
+	// When a player disconnects
+	socket.on("disconnect", () => {
+		console.log("disconnected");
+	});
 });
